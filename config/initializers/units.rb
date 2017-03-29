849507412e9cea0d2379370716cf65739bce48f9
Jaime Alvarez <jaime_andres@unitec.edu>
2017-03-29 06:52:17 +0000

    Initializer with dictionary of used units for each test item

---
+++ config/initializers/units.rb
@@ -0,0 +1,9 @@
+TEST_UNITS = {
+  :systolic_blood_pressure => 'mmHg',
+  :diastolic_blood_pressure => 'mmHg',
+  :blood_glucose => 'mg/dl',
+
+  :total_cholesterol => 'mg/dl',
+  :hdl_cholesterol => 'mg/dl',
+  :ldl_cholesterol => 'mg/dl'
+}
