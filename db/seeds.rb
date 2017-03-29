8602cc66d4efbfe60c9a67e8e5a2d022eb42a354
Jaime Alvarez <jaime_andres@unitec.edu>
2017-03-29 03:29:45 +0000

    Basic seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create(email: 'test@test.com', password: '12345678', password_confirmation: '12345678')
+
+Entry.create(systolic_blood_pressure: 116,
+            diastolic_blood_pressure: 52,
+            total_cholesterol: 226,
+            hdl_cholesterol: 53,
+            ldl_cholesterol: 161,
+            blood_glucose: 82,
+            user: User.first)
