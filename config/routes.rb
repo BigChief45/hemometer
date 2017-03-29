189b05265c5fec6551929b1ba44d9a9be5a5c104
Jaime Alvarez <jaime_andres@unitec.edu>
2017-03-29 02:58:54 +0000

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+
+  devise_for :users
+  devise_scope :user do
+    get 'register', to: 'devise/registrations#new'
+    get 'login', to: 'devise/sessions#new'
+    delete 'logout', to: 'devise/sessions#destroy'
+  end
+
+  resources :entries
+
+  root 'entries#index'
+end
---
+++ db/migrate/20170315034026_create_entries.rb
@@ -0,0 +1,20 @@
+class CreateEntries < ActiveRecord::Migration[5.0]
+  def change
+    create_table :entries do |t|
+      t.datetime :date
+      t.text :notes
+      t.decimal :red_blood_cell
+      t.decimal :white_blood_cell
+      t.decimal :platelets
+      t.decimal :hemoglobin
+      t.decimal :blood_glucose
+      t.decimal :total_cholesterol
+      t.decimal :triglyceride
+      t.decimal :ldl_cholesterol
+      t.decimal :hdl_cholesterol
+      t.decimal :weight
+
+      t.timestamps
+    end
+  end
+end
