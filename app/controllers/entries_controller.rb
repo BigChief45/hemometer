eaf4655c72c0cee35b7800096f5554ee66175a33
Jaime Alvarez <jaime_andres@unitec.edu>
2017-03-29 03:29:16 +0000

    Add basic controller actions

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,2 +1,14 @@
 class EntriesController < ApplicationController
+
+  before_action :authenticate_user!
+
+  def index
+    @entries = current_user.entries.all
+    @last_entry = @entries.last
+  end
+
+  def show
+    @entry = Entry.find(params[:id])
+  end
+
 end
